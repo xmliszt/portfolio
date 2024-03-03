b0471c407d659aec043e927b38c4f28f6402a459
Li Yuxuan <xmliszt@gmail.com>
2024-03-03 23:58:34 +0800

    fix piano layout

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <h1 className='text-center font-bold'>
         No page here. Why not enjoy some music?
       </h1>
-      <div className='mt-32 max-w-fit rotate-90 rounded-lg bg-stone-700 p-4 shadow-[inset_0_0_20px_10px_rgba(0,0,0,0.4),0_0_20px_10px_rgba(0,0,0,0.4)] md:rotate-0'>
+      <div className='my-32 w-max rotate-90 rounded-lg bg-stone-700 p-4 shadow-[inset_0_0_20px_10px_rgba(0,0,0,0.4),0_0_20px_10px_rgba(0,0,0,0.4)] md:my-8 md:rotate-0'>
         <Piano />
       </div>
     </div>
