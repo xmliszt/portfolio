9e46556fd2319e7f8de859dab203b934d3134afd
Li Yuxuan <xmliszt@gmail.com>
2024-04-01 23:37:24 +0800

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,107 @@
+import { MetadataRoute } from 'next';
+
+import { focus, hobbies, posts, tags } from '#site/content';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteMapContent: MetadataRoute.Sitemap = [
+    {
+      url: 'https://liyuxuan.dev/about',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: 'https://liyuxuan.dev/posts',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: 'https://liyuxuan.dev/tags',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: 'https://liyuxuan.dev/playground',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: 'https://liyuxuan.dev/hobbies',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: 'https://liyuxuan.dev/focus',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: 'https://liyuxuan.dev/the-why',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: 'https://liyuxuan.dev/contacts',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+  ];
+
+  const postsSiteMap: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `https://liyuxuan.dev/posts/${post.slug}`,
+    lastModified: new Date(post.date),
+    changeFrequency: 'weekly',
+    priority: 1,
+  }));
+
+  const hobbiesSiteMap: MetadataRoute.Sitemap = hobbies.map((hobby) => ({
+    url: `https://liyuxuan.dev/hobbies/${hobby.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 1,
+  }));
+
+  const focusSiteMap: MetadataRoute.Sitemap = focus.map((focus) => ({
+    url: `https://liyuxuan.dev/focus/${focus.slug}`,
+    lastModified: focus.updated ? new Date(focus.updated) : new Date(),
+    changeFrequency: 'monthly',
+    priority: 1,
+  }));
+
+  const tagsSiteMap: MetadataRoute.Sitemap = tags.map((tag) => ({
+    url: `https://liyuxuan.dev/tags/${tag}`,
+    lastModified: new Date(),
+    changeFrequency: 'daily',
+    priority: 1,
+  }));
+
+  const experimentsSiteMap: MetadataRoute.Sitemap = [
+    {
+      url: 'https://liyuxuan.dev/experiments/piano',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: 'https://liyuxuan.dev/experiments/dune-themed-light-dark-toggle',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+  ];
+
+  return siteMapContent.concat(
+    postsSiteMap,
+    hobbiesSiteMap,
+    focusSiteMap,
+    experimentsSiteMap,
+    tagsSiteMap
+  );
+}
