ddc82bcbcc418a58f05d527cca0a653199fece6b
Li Yuxuan <xmliszt@gmail.com>
2025-03-23 09:47:04 +0800

    Added prettier format on save support

--- app/craft/components/sandbox/sandbox.tsx
+++ app/craft/components/sandbox/sandbox.tsx
@@ -1,12 +1,25 @@
 'use client';
 
+import { useCallback, useEffect, useState } from 'react';
+import { autocompletion } from '@codemirror/autocomplete';
+import { javascript } from '@codemirror/lang-javascript';
 import {
   SandpackCodeEditor,
   SandpackLayout,
   SandpackPreview,
   SandpackProvider,
+  useSandpack,
 } from '@codesandbox/sandpack-react';
+import { debounce } from 'lodash';
+import { Check, X } from 'lucide-react';
 import { useTheme } from 'next-themes';
+import prettier from 'prettier';
+import parserBabel from 'prettier/parser-babel';
+import parserHTML from 'prettier/parser-html';
+import parserTS from 'prettier/parser-typescript';
+import prettierPluginEstree from 'prettier/plugins/estree.js';
+
+import parserSCSS from 'prettier/parser-postcss';
 
 type SandboxProps = {
   code: string;
@@ -42,11 +55,7 @@ export function Sandbox(props: SandboxProps) {
       }}
     >
       <SandpackLayout>
-        <SandpackCodeEditor
-          showLineNumbers
-          wrapContent
-          style={{ height: '666px', fontSize: '12px' }}
-        />
+        <EditorWithPrettier />
         <SandpackPreview
           showOpenInCodeSandbox={false}
           style={{ height: '666px' }}
@@ -55,3 +64,137 @@ export function Sandbox(props: SandboxProps) {
     </SandpackProvider>
   );
 }
+
+// Custom Editor Component
+function EditorWithPrettier() {
+  const { prettier } = useIsPrettier();
+
+  return (
+    <>
+      <SandpackCodeEditor
+        showLineNumbers
+        wrapContent
+        extensions={[
+          javascript({ typescript: true, jsx: true }),
+          autocompletion(),
+        ]}
+        style={{ height: '666px', fontSize: '12px' }}
+      />
+      {prettier && <PrettierPlugin />}
+    </>
+  );
+}
+
+// Prettier Plugin Button
+function PrettierPlugin() {
+  const { error, success, prettifyCode } = usePrettier();
+
+  return (
+    <button
+      style={{ color: error ? '#ef4444' : success ? '#22c55e' : '#808080' }}
+      className='bg-background absolute top-0 left-0 z-10 flex items-center gap-x-1 rounded-tl-md px-1.5 py-0.5 text-xs transition-all duration-300'
+      onClick={prettifyCode}
+    >
+      {error ? <X size={12} /> : <Check size={12} />}
+      Prettier
+    </button>
+  );
+}
+
+// Hook to check if Prettier is supported
+const useIsPrettier = () => {
+  const [prettier, setPrettier] = useState(false);
+  const { sandpack } = useSandpack();
+
+  useEffect(() => {
+    const activeFile = sandpack.files[sandpack.activeFile];
+    if (!activeFile) return;
+
+    const fileExtension = sandpack.activeFile.split('.').pop()?.toLowerCase();
+    if (!fileExtension) return;
+
+    const prettierExtensions = [
+      'js',
+      'ts',
+      'jsx',
+      'tsx',
+      'scss',
+      'css',
+      'html',
+    ];
+    const isPrettierSupported = !(
+      activeFile.readOnly || !prettierExtensions.includes(fileExtension)
+    );
+
+    setPrettier(isPrettierSupported);
+  }, [sandpack.files, sandpack.activeFile]);
+
+  return { prettier };
+};
+
+// Hook to handle Prettier formatting
+const usePrettier = () => {
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const { sandpack } = useSandpack();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedUpdate = useCallback(
+    debounce((code: string) => {
+      sandpack.updateCurrentFile(code, false);
+    }, 150),
+    [sandpack.activeFile, sandpack.files]
+  );
+
+  const prettifyCode = useCallback(async () => {
+    const activeFile = sandpack.files[sandpack.activeFile];
+    const currentCode: string = activeFile.code;
+
+    try {
+      const fileExtension = sandpack.activeFile.split('.').pop()?.toLowerCase();
+      let formattedCode: string = currentCode;
+
+      if (fileExtension === 'scss' || fileExtension === 'css') {
+        formattedCode = await prettier.format(currentCode, {
+          parser: 'scss',
+          plugins: [parserSCSS],
+        });
+      } else {
+        formattedCode = await prettier.format(currentCode, {
+          parser:
+            fileExtension === 'ts' || fileExtension === 'tsx'
+              ? 'typescript'
+              : 'babel',
+          plugins: [parserBabel, parserTS, parserHTML, prettierPluginEstree],
+        });
+      }
+
+      setError(false);
+      setSuccess(true);
+      debouncedUpdate(formattedCode);
+    } catch (error) {
+      setError(true);
+      console.error(error);
+    } finally {
+      setTimeout(() => {
+        setSuccess(false);
+      }, 500);
+    }
+  }, [debouncedUpdate, sandpack.activeFile, sandpack.files]);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if ((event.ctrlKey || event.metaKey) && event.key === 's') {
+        event.preventDefault();
+        prettifyCode();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [sandpack.files, sandpack.activeFile, prettifyCode]);
+
+  return { error, success, prettifyCode };
+};
