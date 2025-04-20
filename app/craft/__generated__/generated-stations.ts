8879775b99c0023624384662b04bf7d4f1332180
Li Yuxuan <xmliszt@gmail.com>
2025-04-20 14:06:34 +0800

    Update to remvoe animate button, make the switch automatic in fixed interval

--- app/craft/__generated__/generated-stations.ts
+++ app/craft/__generated__/generated-stations.ts
@@ -344,8 +344,7 @@ p {
 `,
   },
   'text-morph-effect': {
-    'App.tsx': `import { useMemo, useState } from 'react';
-import { debounce } from 'lodash';
+    'App.tsx': `import { useEffect, useState } from 'react';
 import { PlaneTakeoff } from 'lucide-react';
 import { AnimatePresence, motion } from 'motion/react';
 
@@ -393,34 +392,36 @@ export default function App() {
   const [newText, setNewText] = useState<string>('Boarding');
   const [displayText, setDisplayText] = useState<string>(oldText);
 
-  const updateDisplayTextDebouncedly = useMemo(
-    () => debounce((text: string) => setDisplayText(text), 500),
-    []
-  );
+  const INTERVAL = 2000;
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (displayText === oldText) setDisplayText(newText);
+      else setDisplayText(oldText);
+    }, INTERVAL);
+
+    return () => clearInterval(intervalId);
+  }, [oldText, newText, displayText]);
 
   return (
     <div className='flex h-screen w-screen items-center justify-center font-bold'>
       <div className='flex flex-col gap-y-8 p-2'>
         {/* Text display */}
-        <div className='flex h-32 w-full items-center justify-center rounded-md bg-neutral-100 p-4 shadow-inner select-none'>
+        <div className='flex h-32 w-full items-center justify-center rounded-xl bg-neutral-100 p-4 shadow-inner select-none'>
           <div className='relative flex h-16 w-64 items-center justify-center rounded-xl bg-neutral-800 px-3 py-1.5 text-center text-lg text-neutral-100 shadow-lg'>
             <MorphText>{displayText}</MorphText>
             <PlaneTakeoff className='absolute top-2 left-2 size-4' />
           </div>
         </div>
-        <div className='flex w-full flex-col gap-x-4 gap-y-2 sm:flex-row'>
+        <div className='flex w-full flex-col gap-x-4 gap-y-4 sm:flex-row'>
           <div className='flex flex-col gap-y-1.5'>
             <label htmlFor='before-input' className='text-sm'>
               Before
             </label>
             <input
               id='before-input'
-              className='h-10 w-full rounded-md bg-neutral-100 px-2.5 transition-all focus-visible:ring focus-visible:outline-none sm:w-64'
+              className='h-10 w-full rounded-md bg-neutral-100 px-2.5 transition-all focus-visible:ring focus-visible:ring-neutral-800 focus-visible:outline-none sm:w-64'
               value={oldText}
-              onChange={(e) => {
-                setOldText(e.target.value);
-                updateDisplayTextDebouncedly(e.target.value);
-              }}
+              onChange={(e) => setOldText(e.target.value)}
               maxLength={16}
             />
           </div>
@@ -430,26 +431,12 @@ export default function App() {
             </label>
             <input
               id='after-input'
-              className='h-10 w-full rounded-md bg-neutral-100 px-2.5 transition-all focus-visible:ring focus-visible:outline-none sm:w-64'
+              className='h-10 w-full rounded-md bg-neutral-100 px-2.5 transition-all focus-visible:ring focus-visible:ring-neutral-800 focus-visible:outline-none sm:w-64'
               value={newText}
               onChange={(e) => setNewText(e.target.value)}
               maxLength={16}
             />
           </div>
-        </div>
-        <div>
-          <motion.button
-            className='cursor-default rounded-md bg-orange-400 px-3 py-1.5 text-neutral-100 shadow-lg'
-            initial={{ opacity: 1 }}
-            whileHover={{ opacity: 0.8 }}
-            whileTap={{ opacity: 1 }}
-            onClick={() => {
-              setDisplayText(newText);
-              setTimeout(() => updateDisplayTextDebouncedly(oldText), 1000);
-            }}
-          >
-            Animate
-          </motion.button>
         </div>
       </div>
     </div>
