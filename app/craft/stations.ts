f8f0d72c8f096a395a8e4926197b4b88e8bdeee6
Li Yuxuan <xmliszt@gmail.com>
2025-04-20 14:25:33 +0800

    Update copy

--- app/craft/stations.ts
+++ app/craft/stations.ts
@@ -4,7 +4,7 @@ import type { CraftStation } from "./types";
 export const stations: CraftStation[] = [
   {
     id: "apple-vision-resize-handle",
-    title: "Apple Vision OS resize handle animation",
+    title: "vision OS resize handle animation",
     tags: ["AnimatePresence", "MotionConfig", "useDragControls"],
     description:
       "Use react-motion to re-create the Apple Vision OS window resize handle animation with real resizing functionality.",
@@ -19,14 +19,14 @@ export const stations: CraftStation[] = [
   },
   {
     id: "simple-stagger-paragraphs-enter-animation",
-    title: "Simple stagger paragraphs enter animation",
+    title: "simple stagger paragraphs enter animation",
     tags: ["keyframes", "animation-delay", "animation-fill-mode"],
     description: `Use simple CSS keyframe animation with CSS variable to create a staggered entering animation for paragraphs. Added subtle blur animation to make it look more natural. This simple yet elegant transition is inspired by Paco Coursey's portfolio site: https://paco.me/.`,
     codes: generatedStations["simple-stagger-paragraphs-enter-animation"],
   },
   {
     id: "text-morph-effect",
-    title: "Text morph effect",
+    title: "text morph effect",
     tags: ["AnimatePresence", "layoutId"],
     description: `Use shared layout from React Motion, we are able to create text morphing effect that adds an interesting touch and draws users attentions to the change in text. This is a learning practice from Emil Kowalski's course: Animations on the web.`,
     codes: generatedStations["text-morph-effect"],
