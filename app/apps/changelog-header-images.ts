e95a3e9940b71d39c54838b2e920d9ffca8b1b1e
Li Yuxuan <xmliszt@gmail.com>
2026-06-01 11:44:25 +0800

    feat: use cloudflare R2 for changelog cdn serving

--- app/apps/changelog-header-images.ts
+++ app/apps/changelog-header-images.ts
@@ -7,72 +7,40 @@ export const CHANGELOG_HEADER_IMAGES: Record<
 > = {
   joodle: {
     "2.0.163": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/2.0_1.gif",
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/2.0_2.gif",
+      "https://joodle.liyuxuan.dev/changelogs/2.0_1.gif",
+      "https://joodle.liyuxuan.dev/changelogs/2.0_2.gif",
     ],
     "1.18.145": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.18_1.png",
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.18_2.png",
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.18_3.gif",
+      "https://joodle.liyuxuan.dev/changelogs/1.18_1.png",
+      "https://joodle.liyuxuan.dev/changelogs/1.18_2.png",
+      "https://joodle.liyuxuan.dev/changelogs/1.18_3.gif",
     ],
-    "1.17.141": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.17.png",
-    ],
-    "1.16.116": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.16.gif",
-    ],
-    "1.15.114": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.15.gif",
-    ],
-    "1.14.111": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.14.png",
-    ],
+    "1.17.141": ["https://joodle.liyuxuan.dev/changelogs/1.17.png"],
+    "1.16.116": ["https://joodle.liyuxuan.dev/changelogs/1.16.gif"],
+    "1.15.114": ["https://joodle.liyuxuan.dev/changelogs/1.15.gif"],
+    "1.14.111": ["https://joodle.liyuxuan.dev/changelogs/1.14.png"],
     // App store version 1.12 release
-    "1.12.102": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.12.gif",
-    ],
+    "1.12.102": ["https://joodle.liyuxuan.dev/changelogs/1.12.gif"],
     // App store version 1.11 release
-    "1.11.99": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.11.png",
-    ],
+    "1.11.99": ["https://joodle.liyuxuan.dev/changelogs/1.11.png"],
     // App store version 1.10 release
-    "1.10.98": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.10.png",
-    ],
+    "1.10.98": ["https://joodle.liyuxuan.dev/changelogs/1.10.png"],
     // App store version 1.8 release
-    "1.8.91": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.8.91.png",
-    ],
+    "1.8.91": ["https://joodle.liyuxuan.dev/changelogs/1.8.91.png"],
     // App store version 1.7 release
-    "1.7.89": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.7.89.gif",
-    ],
+    "1.7.89": ["https://joodle.liyuxuan.dev/changelogs/1.7.89.gif"],
     // App store version 1.5 release
-    "1.5.83": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.5.83.gif",
-    ],
+    "1.5.83": ["https://joodle.liyuxuan.dev/changelogs/1.5.83.gif"],
     // App store version 1.4 release
-    "1.4.79": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.4.79.gif",
-    ],
+    "1.4.79": ["https://joodle.liyuxuan.dev/changelogs/1.4.79.gif"],
     // App store version 1.3 release
-    "1.3.78": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.3.78.gif",
-    ],
+    "1.3.78": ["https://joodle.liyuxuan.dev/changelogs/1.3.78.gif"],
     // App store version 1.1 release
-    "1.1.73": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.1.73.gif",
-    ],
+    "1.1.73": ["https://joodle.liyuxuan.dev/changelogs/1.1.73.gif"],
     // TestFlight last release
-    "1.1.68": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.1.68.gif",
-    ],
-    "1.0.58": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.0.58.png",
-    ],
-    "1.0.54": [
-      "https://raw.githubusercontent.com/xmliszt/resources/refs/heads/main/joodle/1.0.54.png",
-    ],
+    "1.1.68": ["https://joodle.liyuxuan.dev/changelogs/1.1.68.gif"],
+    "1.0.58": ["https://joodle.liyuxuan.dev/changelogs/1.0.58.png"],
+    "1.0.54": ["https://joodle.liyuxuan.dev/changelogs/1.0.54.png"],
   },
 };
 
